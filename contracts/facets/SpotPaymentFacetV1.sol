682c673f52ce8cdca787a12a6dee3778a28a20db
Nas J <nas@hashleap.io>
2023-04-15 13:22:51 +0100

    Renames SpotPaymentFacett to SpotPaymentFacetV1

--- contracts/facets/SpotPaymentFacetV1.sol
+++ contracts/facets/SpotPaymentFacetV1.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.14;
 
-contract SpotPaymentFacet {
+contract SpotPaymentFacetV1 {
     event TestEvent(address something);
 
     function test1Func1() external pure returns(uint a){
